e4b05871abb90813eb201a9dd569a534f3560e4b
Ivana Drábiková <idrabikova@gmail.com>
2014-12-11 22:33:10 +0100

    links between github and buttons

--- active_english_vocabulary.py
+++ active_english_vocabulary.py
@@ -32,12 +32,35 @@ class ActiveEnglishVocabulary(Frame):
 
     def create_widgets(self):
         """ Create buttons for words to open their translation in browser. """
-        self.button1 = Button(self, text = "See in dictionary WORD", command=self.open_url)
+        self.button1 = Button(self, text = "Online AEV Dictionary", command=self.open_url)
         self.button1.grid()
 
     def open_url(self):
         webbrowser.open_new(url)
 
+
+
+class QuitButton(Button):
+    """Button for exiting the application"""
+    def __init__(self, parent):
+        Button.__init__(self, parent)
+        # Change the message here
+        self['text'] = 'See you later!'
+        # Command to close the window (the destory method)
+        self['command'] = parent.destroy
+        self.grid()
+
+
+class AboutButton(Button):
+    """Button redirecting new tab to my GitHub"""
+    def __init__(self, parent):
+        Button.__init__(self, parent)
+        self['text'] = 'Take me to the source code'
+        self['command'] = self.open_about_page
+        self.grid()
+
+    def open_about_page(self):
+        webbrowser.open_new("https://github.com/Atenna/ActiveEnglishVocabulary")
 
 def process_data():
     """Convert Twitter stream into dictionary of word - frequency pair"""
@@ -73,14 +96,19 @@ def lines(fp):
 def write_to_file():
     """Loop over dictionary sorted by frequency and write to file"""
     text_file = open("absolute_frequency.txt","w")
+    html_file = open("absolute_frequency.html", "w")
     d_view = [(v,k) for k,v in unknown.iteritems()]
     d_view.sort(reverse=True)
+    html_file.write("<ul>\n")
     for v,k in d_view:
         if k != "":
+            html_file.write("<li> %s: %d \t" % (k,v))
             text_file.write("%s: %d \t" % (k,v))
-            text_file.write("http://slovnik.azet.sk/preklad/anglicko-slovensky/?q="+k)
-            text_file.write("\n")
+            html_file.write("<a href='http://slovnik.azet.sk/preklad/anglicko-slovensky/?q="+k+"'Slovnik</a></li>\n")
+            text_file.write("http://slovnik.azet.sk/preklad/anglicko-slovensky/?q="+k+"\n")
+    html_file.write("</ul>")
     text_file.close()
+    html_file.close()
 
 
 def calculate_sentiment(tweet, scores):
@@ -107,7 +135,9 @@ if __name__ == '__main__':
     process_data()
     write_to_file()
     root = Tk()
-    root.title("Active English Vocabulary")
-    root.geometry("500x500")
+    root.title("AEV")
+    root.geometry("192x100")
     app = ActiveEnglishVocabulary(root)
+    AboutButton(root)
+    QuitButton(root)
     root.mainloop()
