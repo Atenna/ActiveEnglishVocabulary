c9aa6c8a4b75c00c674a845987df6170c8344eb6
Ivana Lieska <idrabikova@gmail.com>
2017-01-24 14:53:57 +0100

    v1.3

--- main.py
+++ main.py
@@ -10,32 +10,65 @@ from Tkinter import *
 top = Tk()
 top.wm_title("Active English Vocabulary")
 
-def openMenu(wordClass):
-    if(wordClass == "noun"):
-        print ""
+frames = {}
 
+### Frame setup ###
+
+fIntroFrame = Frame(top)
+fNounFrame = Frame(top)
+fVerbFrame = Frame(top)
+fAdverbFrame = Frame(top)
+fAdjectiveFrame = Frame(top)
+fOthersFrame = Frame(top)
+fAboutFrame = Frame(top)
+fCodeFrame = Frame(top)
+
+Button(fNounFrame, text='Go to frame 2', command=lambda:openTab("verb")).pack()
+Label(fNounFrame, text='FRAME 1').pack()
+
+Button(fVerbFrame, text='Go to frame 1', command=lambda:openTab("noun")).pack()
+Label(fVerbFrame, text='FRAME 2').pack()
+
+frames["noun"] = fNounFrame
+frames["verb"] = fVerbFrame
+frames["adverb"] = fAdverbFrame
+frames["adjective"] = fAdjectiveFrame
+frames["others"] = fOthersFrame
+frames["about"] = fAboutFrame
+frames["code"]= fCodeFrame
+
+for f in (fIntroFrame,fNounFrame,fVerbFrame,fAdverbFrame,fAdjectiveFrame,fOthersFrame,fAboutFrame,fCodeFrame):
+    f.grid(row=0, column=0, sticky='news')
+
+def openTab(frameName):
+    frame = frames[frameName]
+    frame.tkraise()
+
+### Menu setup ###
 
 mMenuBar = Menu(top)
 
 # Learn menu: Nouns, Verbs, Adjectives, Adverbs
 mLearnMenu = Menu(mMenuBar, tearoff=0)
-mLearnMenu.add_command(label="Nouns", command=openMenu("noun"))
-mLearnMenu.add_command(label="Verbs", command=openMenu("verb"))
-mLearnMenu.add_command(label="Adjectives", command=openMenu("adjective"))
-mLearnMenu.add_command(label="Adverbs", command=openMenu("adverb"))
-mLearnMenu.add_command(label="Others", command=openMenu("others"))
+mLearnMenu.add_command(label="Nouns", command=openTab("noun"))
+mLearnMenu.add_command(label="Verbs", command=openTab("verb"))
+mLearnMenu.add_command(label="Adjectives", command=openTab("adjective"))
+mLearnMenu.add_command(label="Adverbs", command=openTab("adverb"))
+mLearnMenu.add_command(label="Others", command=openTab("others"))
 
 # About menu: About, Code
 mAboutMenu = Menu(mMenuBar, tearoff=0)
-mAboutMenu.add_command(label="About", command=openMenu("about"))
-mAboutMenu.add_command(label="Code", command=openMenu("code"))
+mAboutMenu.add_command(label="About", command=openTab("about"))
+mAboutMenu.add_command(label="Code", command=openTab("code"))
 
 # Add options to the menu bar
 mMenuBar.add_cascade(label="Learn", menu=mLearnMenu)
 mMenuBar.add_cascade(label="About", menu=mAboutMenu)
 
+### Table setup ###
+
 # Create treeview with sample data
-tTable = ttk.Treeview(top)
+tTable = ttk.Treeview(fAboutFrame)
 tTable['columns'] = ('word', 'translation')
 tTable.column('#0', width=0, minwidth=0)
 tTable.heading('word', text='Word')
@@ -56,4 +89,5 @@ tTable.insert('', 'end', values=('C B'))
 
 top.config(menu=mMenuBar)
 tTable.pack(side='top')
+openTab("noun")
 top.mainloop()
